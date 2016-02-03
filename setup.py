2cc5d33db275ccd3579c02676d9284bfc21d9b22
shenning_00@yahoo.com <shenning_00@yahoo.com>
2016-02-03 17:47:12 -0600

    Added ability to invoke test to the setup

--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 
 setup(
-    author="@ScottHenning",
+    tests_require=['pytest'],
+    cmdclass = { 'test' : PyTest },
+    athor="@ScottHenning",
     author_email="shenning@cisco.com",
     description="A Python wrapper for Spark Developers API",
     install_requires=["requests"],
